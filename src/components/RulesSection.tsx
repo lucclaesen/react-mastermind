b5447f94f21dbeeee457ce7aec34901ef14ab68f
lucclaesen <luc.claesen@gmail.com>
2016-12-01 01:04:12 +0100

    Edited the rules text

--- src/components/RulesSection.tsx
+++ src/components/RulesSection.tsx
@@ -34,10 +34,29 @@ export default class RulesSection extends React.Component<any, RulesSectionState
                         }
                     }
                 >
-                    Rules ...
+                    Rules
                 </a>
                 <p style={{display: displayValue}}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum magnam reiciendis, qui veritatis id ducimus nam facilis molestias praesentium accusamus tempore nihil, ullam! Voluptate odit saepe, commodi accusantium voluptatem ducimus!
+                    <p>
+                    The goal of the game is to guess the combination of colors that was randomly choosen at a the outset. I.e. a player needs to determine the right color on the 
+                    right position. A color may occur more than once in the target combination.
+                    </p>
+                    <p>
+                    You loose the game if the solution cannot be guessed within 10 rounds.
+                    </p>
+                    <p>
+                    After every guess, you'll get feedback on how your guess relates to the target solution:
+                    <ul>
+                        <li>
+                            <span className="fed fed-exact"></span> means that you placed one color at the correct position.
+                        </li>
+                        <li><span className="fed"></span> indicates that a color occurs in the solution, but in a different location.
+                        </li>
+                        <li><span className="fed fed-miss"></span> means that a color that occurs in your guess does not occur in the solution.
+                        </li>
+                    </ul>
+                    </p>                    
+
                 </p>
             </div>   
         );
